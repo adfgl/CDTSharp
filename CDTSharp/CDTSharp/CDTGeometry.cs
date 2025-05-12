e091f36e1b6f61123a7ac3bf8bfe9708e57df514
Pavel Zhukov <pavel.zhukov.engineer@gmail.com>
2025-05-12 16:50:38 +0300

    add triangle structure

---
+++ CDTSharp/CDTSharp/CDTGeometry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDTSharp
+{
+    public static class CDTGeometry
+    {
+        public const int NO_INDEX = -1;
+    }
+}
---
+++ CDTSharp/CDTSharp/CDTTriangle.cs
@@ -0,0 +1,45 @@
+﻿namespace CDTSharp
+{
+    using static CDTGeometry;
+
+    public struct CDTTriangle
+    {
+        public int index;
+        public int a, b, c;
+        public int abAdj, bcAdj, caAdj;
+        public bool abCon, bcCon, caCon;
+
+        public CDTTriangle(int index, 
+            int a, int b, int c, 
+            int abAdj = NO_INDEX, int bcAdj = NO_INDEX, int caAdj = NO_INDEX)
+        {
+            this.index = index;
+
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
+            this.abAdj = abAdj;
+            this.bcAdj = bcAdj;
+            this.caAdj = caAdj;
+        }
+
+        public int IndexOf(int v)
+        {
+            if (a == v) return 0;
+            if (b == v) return 1;
+            if (c == v) return 2;
+            return NO_INDEX;
+        }
+
+        public int IndexOf(int from, int to)
+        {
+            if (a == from && b == to) return 0;
+            if (b == from && c == to) return 1;
+            if (c == from && a == to) return 2;
+            return NO_INDEX;
+        }
+    }
+
+
+}
