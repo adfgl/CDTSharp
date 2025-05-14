45566f3ed8f5454634fe7c3af0ceb340042c6c3e
Pavel Zhukov <pavel.zhukov.engineer@gmail.com>
2025-05-14 08:34:29 +0300

    write circle tests

--- CDTSharp/CDTSharpTests/CircleTests.cs
+++ CDTSharp/CDTSharpTests/CircleTests.cs
@@ -10,22 +10,65 @@ namespace CDTSharpTests
     public class CircleTests
     {
         [Fact]
-        public void CircleBuildsCorrectlyFromThreeSparslyPositionedPoints()
+        public void CircleBuildsCorrectly_FromThreeSparslyPositionedPoints()
         {
             Vec2 a = new Vec2(79.144, 143.238);
             Vec2 b = new Vec2(170.708, 197.264);
             Vec2 c = new Vec2(199.125, 103.244);
 
-
-            var circle = new Circle(a, b, c);
-            Assert.False(Double.IsNaN(circle.x));
-            Assert.False(Double.IsNaN(circle.y));
-            Assert.False(Double.IsNaN(circle.radiusSquared));
+            Circle circle = new Circle(a, b, c);
 
             const double epsilon = 1e-3;
             Assert.Equal(144.018, circle.x, epsilon);
             Assert.Equal(137.893, circle.y, epsilon);
             Assert.Equal(65.094, Math.Sqrt(circle.radiusSquared), epsilon);
+        }
+
+        [Fact]
+        public void CircleBuildsCorrectly_FromTwoUniquePoints()
+        {
+            Vec2 a = new Vec2(79.144, 143.238);
+            Vec2 b = new Vec2(199.125, 103.244);
+
+            Circle circle = new Circle(a, b);
+
+            const double epsilon = 1e-3;
+            Assert.Equal(139.135, circle.x, epsilon);
+            Assert.Equal(123.241, circle.y, epsilon);
+            Assert.Equal(63.236, Math.Sqrt(circle.radiusSquared), epsilon);
+        }
+
+        [Fact]
+        public void CircleContains_PointStrictlyInside()
+        {
+            Vec2 a = new Vec2(-50, 0);
+            Vec2 b = new Vec2(+50, 0);
+
+            Circle circle = new Circle(a, b);
+
+            Assert.True(circle.Contains(25, 25));
+        }
+
+        [Fact]
+        public void CircleContains_PointStrictlyOutside()
+        {
+            Vec2 a = new Vec2(-50, 0);
+            Vec2 b = new Vec2(+50, 0);
+
+            Circle circle = new Circle(a, b);
+
+            Assert.False(circle.Contains(125, 25));
+        }
+
+        [Fact]
+        public void CircleContains_PointOnCircumference()
+        {
+            Vec2 a = new Vec2(-50, 0);
+            Vec2 b = new Vec2(+50, 0);
+
+            Circle circle = new Circle(a, b);
+
+            Assert.False(circle.Contains(a.x, a.y));
         }
     }
 }
--- CDTSharp/CDTSharpTests/GeometryTests.cs
+++ CDTSharp/CDTSharpTests/GeometryTests.cs
@@ -7,20 +7,7 @@ namespace CDTSharpTests
 
     public class GeometryTests
     {
-        [Fact]
-        public void CircleBuildsCorrectlyFromThreeSparslyPositionedPoints()
-        {
-            Vec2 a = new Vec2(79.144, 143.238);
-            Vec2 b = new Vec2(170.708, 197.264);
-            Vec2 c = new Vec2(199.125, 103.244);
 
-            Assert.True(CircleFromThreePoints(a, b, c, out double cx, out double cy, out double rSqr));
-
-            const double epsilon = 1e-3;
-            Assert.Equal(144.018, cx, epsilon);
-            Assert.Equal(137.893, cy, epsilon);
-            Assert.Equal(65.094, Math.Sqrt(rSqr), epsilon);
-        }
 
         [Fact]
         public void TriangleSplitCenterSplitWorksCorrectlyWithNoNeighbours()
