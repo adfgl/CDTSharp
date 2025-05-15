007eba180223dd1cbd3d7bee776776e9d12e36c6
Pavel Zhukov <pavel.zhukov.engineer@gmail.com>
2025-05-15 12:06:41 +0300

    write tests for node and edge

--- CDTSharp/CDTSharpTests/PolygonTests.cs
+++ CDTSharp/CDTSharpTests/PolygonTests.cs
@@ -23,5 +23,21 @@ namespace CDTSharpTests
             Polygon poly = Rombus();
             Assert.False(poly.Contains(poly.rect.minX - 100, 0));
         }
+
+        [Fact]
+        public void Contains_ReturnsTrueWhenPointIsNode()
+        {
+            Polygon poly = Rombus();
+            var (x, y) = poly.vertices[0];
+            Assert.True(poly.Contains(x, y));
+        }
+
+        [Fact]
+        public void Contains_ReturnsTrueWhenPointIsOnEdge()
+        {
+            Polygon poly = Rombus();
+            var (x, y) = Vec2.MidPoint(poly.vertices[0], poly.vertices[1]);
+            Assert.True(poly.Contains(x, y));
+        }
     }
 }
