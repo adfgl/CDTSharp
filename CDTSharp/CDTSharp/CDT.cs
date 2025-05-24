fc58d55f3e3361e81f467d9fa26ff923cfd38a36
adfgl <69821550+adfgl@users.noreply.github.com>
2025-05-24 13:16:48 +0300

    Update CDT.cs
    Move helper functions to bottom of the file

--- CDTSharp/CDTSharp/CDT.cs
+++ CDTSharp/CDTSharp/CDT.cs
@@ -408,86 +408,7 @@ namespace CDTSharp
                 _t[i] = tri;
             }
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool Intersect(Vec2 p1, Vec2 p2, Vec2 q1, Vec2 q2, out Vec2 intersection)
-        {
-            // P(u) = p1 + u * (p2 - p1)
-            // Q(v) = q1 + v * (q2 - q1)
-
-            // goal to vind such 'u' and 'v' so:
-            // p1 + u * (p2 - p1) = q1 + v * (q2 - q1)
-            // which is:
-            // u * (p2x - p1x) - v * (q2x - q1x) = q1x - p1x
-            // u * (p2y - p1y) - v * (q2y - q1y) = q1y - p1y
-
-            // | p2x - p1x  -(q2x - q1x) | *  | u | =  | q1x - p1x |
-            // | p2y - p1y  -(q2y - q1y) |    | v |    | q1y - p1y |
-
-            // | a  b | * | u | = | e |
-            // | c  d |   | v |   | f |
-
-            intersection = Vec2.NaN;
-
-            double a = p2.x - p1.x, b = q1.x - q2.x;
-            double c = p2.y - p1.y, d = q1.y - q2.y;
-
-            double det = a * d - b * c;
-            if (Math.Abs(det) < 1e-12)
-            {
-                return false;
-            }
-
-            double e = q1.x - p1.x, f = q1.y - p1.y;
-
-            double u = (e * d - b * f) / det;
-            double v = (a * f - e * c) / det;
-            if (u < 0 || u > 1 || v < 0 || v > 1)
-            {
-                return false;
-            }
-            intersection = new Vec2(p1.x + u * a, p1.y + u * c);
-            return true;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool OnSegment(Vec2 a, Vec2 b, Vec2 p, double epsilon)
-        {
-            double dxAB = b.x - a.x;
-            double dyAB = b.y - a.y;
-            double dxAP = p.x - a.x;
-            double dyAP = p.y - a.y;
-
-            double cross = dxAB * dyAP - dyAB * dxAP;
-            if (Math.Abs(cross) > epsilon)
-                return false;
-
-            double dot = dxAP * dxAB + dyAP * dyAB;
-            if (dot < 0) return false;
-
-            double lenSq = dxAB * dxAB + dyAB * dyAB;
-            if (dot > lenSq) return false;
-
-            return true;
-
-            double sqrA = (a - p).Length();
-            double sqrB = (b - p).Length();
-            double sqr = (a - b).Length();
-            return Math.Abs(sqrA + sqrB - sqr) <= epsilon;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool ConvexQuad(Vec2 a, Vec2 b, Vec2 c, Vec2 d)
-        {
-            return SameSide(a, b, c, d) && SameSide(d, c, b, a);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static bool SameSide(Vec2 a, Vec2 b, Vec2 c, Vec2 d)
-        {
-            return Vec2.Cross(a, b, c) * Vec2.Cross(c, d, a) >= 0;
-        }
-
+        
         public void SplitEdge(int triangleIndex, int edgeIndex, int vertexIndex)
         {
             /*
@@ -1039,6 +960,85 @@ namespace CDTSharp
             Vec2 b = _v[t.indices[1]];
             Vec2 c = _v[t.indices[2]];
             return Clockwise(a, b, c);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Intersect(Vec2 p1, Vec2 p2, Vec2 q1, Vec2 q2, out Vec2 intersection)
+        {
+            // P(u) = p1 + u * (p2 - p1)
+            // Q(v) = q1 + v * (q2 - q1)
+
+            // goal to vind such 'u' and 'v' so:
+            // p1 + u * (p2 - p1) = q1 + v * (q2 - q1)
+            // which is:
+            // u * (p2x - p1x) - v * (q2x - q1x) = q1x - p1x
+            // u * (p2y - p1y) - v * (q2y - q1y) = q1y - p1y
+
+            // | p2x - p1x  -(q2x - q1x) | *  | u | =  | q1x - p1x |
+            // | p2y - p1y  -(q2y - q1y) |    | v |    | q1y - p1y |
+
+            // | a  b | * | u | = | e |
+            // | c  d |   | v |   | f |
+
+            intersection = Vec2.NaN;
+
+            double a = p2.x - p1.x, b = q1.x - q2.x;
+            double c = p2.y - p1.y, d = q1.y - q2.y;
+
+            double det = a * d - b * c;
+            if (Math.Abs(det) < 1e-12)
+            {
+                return false;
+            }
+
+            double e = q1.x - p1.x, f = q1.y - p1.y;
+
+            double u = (e * d - b * f) / det;
+            double v = (a * f - e * c) / det;
+            if (u < 0 || u > 1 || v < 0 || v > 1)
+            {
+                return false;
+            }
+            intersection = new Vec2(p1.x + u * a, p1.y + u * c);
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool OnSegment(Vec2 a, Vec2 b, Vec2 p, double epsilon)
+        {
+            double dxAB = b.x - a.x;
+            double dyAB = b.y - a.y;
+            double dxAP = p.x - a.x;
+            double dyAP = p.y - a.y;
+
+            double cross = dxAB * dyAP - dyAB * dxAP;
+            if (Math.Abs(cross) > epsilon)
+                return false;
+
+            double dot = dxAP * dxAB + dyAP * dyAB;
+            if (dot < 0) return false;
+
+            double lenSq = dxAB * dxAB + dyAB * dyAB;
+            if (dot > lenSq) return false;
+
+            return true;
+
+            double sqrA = (a - p).Length();
+            double sqrB = (b - p).Length();
+            double sqr = (a - b).Length();
+            return Math.Abs(sqrA + sqrB - sqr) <= epsilon;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool ConvexQuad(Vec2 a, Vec2 b, Vec2 c, Vec2 d)
+        {
+            return SameSide(a, b, c, d) && SameSide(d, c, b, a);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static bool SameSide(Vec2 a, Vec2 b, Vec2 c, Vec2 d)
+        {
+            return Vec2.Cross(a, b, c) * Vec2.Cross(c, d, a) >= 0;
         }
     }
 }
